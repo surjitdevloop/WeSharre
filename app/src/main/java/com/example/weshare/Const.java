ff3fd6e8e9e518d3794b78f9e27b76f52fdf9a56
Surjit Singh <surjitdevloop@gmail.com>
2020-04-14 11:30:48 +0530

    final finsih

--- app/src/main/java/com/example/weshare/Const.java
+++ app/src/main/java/com/example/weshare/Const.java
@@ -1,7 +1,8 @@
 package com.example.weshare;
 
 public class Const {
-    public static final String COLLECTION_PATH = "and2";
-    public static final String COLLECTION_ORDER = "url";
+
+    public static final String COLLECTION_PATH = "videoList";
+    public static final String COLLECTION_ORDER = "timestamp";
 
 }
